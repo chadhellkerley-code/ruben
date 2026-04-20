eb493bb71da3c40a7bcc0c066af209c8fbef6939
chadhellkerley-code <chadhellkerley@icloud.com>
2026-04-20 19:08:55 -0300

    Connect stock app to Firebase Realtime Database

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+window.NICOLA_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAExjYvUeET0m4h2JcSaEIroBfuCh-YJ7g",
+  authDomain: "ruben-3c8f7.firebaseapp.com",
+  databaseURL: "https://ruben-3c8f7-default-rtdb.firebaseio.com",
+  projectId: "ruben-3c8f7",
+  storageBucket: "ruben-3c8f7.firebasestorage.app",
+  messagingSenderId: "217206603541",
+  appId: "1:217206603541:web:c70b6e3ecd1ba50771331c"
+};
+
+window.NICOLA_FIREBASE_DB_PATH = "apps/corralonNicola/estado";
